82ee6d67a352c15396e54e6d97b474e85bf26e2c
Denis <sclad007@mail.ru>
2017-07-09 15:13:30 +0300

    Start

---
+++ arial_10.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+Font created by the LCD Vision V1.05 font & image editor/converter
+(C) Copyright 2011-2013 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Font name: Arial
+The font is proportional (has variable width).
+Font height: 19 pixels
+First character: 0x20
+Last character: 0x7F
+
+Exported font data size:
+3159 bytes for displays organized as horizontal rows of bytes
+2611 bytes for displays organized as rows of vertical bytes.
+****************************************************************************/
+
+#ifndef _ARIAL_10_INCLUDED_
+#define _ARIAL_10_INCLUDED_
+
+extern flash unsigned char arial[];
+
+#endif
+
